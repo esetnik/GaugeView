f143c3f53c829d4878dd9ca3e3d2fb32a3bf295d
Ethan Setnik <esetnik@gmail.com>
2017-05-25 15:39:48 -0400

    fix warnings

--- Pod/Classes/GaugeView.swift
+++ Pod/Classes/GaugeView.swift
@@ -190,7 +190,7 @@ open class GaugeView: UIView {
   }
   
   private func convertDegreesToRadius(degrees: Float) -> Float {
-    return ((Float(M_PI) * degrees) / 180.0)
+    return (.pi * degrees) / 180.0
   }
   
 }
